8fc0e23f66dda679b4993d7198dbb03b1ecbcdeb
Marty VanHoof <garchuvius@yahoo.ca>
2017-09-24 14:35:26 -0400

    Update log_reg_scripts.py

--- Logistic_Regression/log_reg_scripts.py
+++ Logistic_Regression/log_reg_scripts.py
@@ -20,3 +20,20 @@ def logReg_cost(theta, X, y):
     J_theta = ( -1/len(y) )*( y.T*ln_h + (1 - y.T)*ln_1h )
     
     return J_theta[0,0]
+
+def logReg_gradient(theta, X, y):
+    '''Compute the gradient of the logistic regression cost function. 
+    Inputs:
+    X is an m by (n+1) numpy array, where m = # of training examples
+                                     n = # of features (excluding the "1" column)
+    y is an m by 1 numpy array
+    theta is a 1 by (n+1) numpy array
+    Output:
+    1 by (n+1) numpy array of partial derivatives'''
+    
+    # transform the inputs to numpy matrices
+    X, y, theta = np.matrix(X), np.matrix(y), np.matrix(theta)
+    
+    h_theta = logistic(X*theta.T)
+    gradient = ( 1 / len(y) )*(h_theta - y).T * X
+    return np.array(gradient)
